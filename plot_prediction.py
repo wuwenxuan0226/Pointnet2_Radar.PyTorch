8340120b0e83459699e69979da3d9b6192908609
wuwenxuan0226 <wwx0471@qq.com>
2023-08-02 15:23:39 +0800

    Use subplot

--- plot_prediction.py
+++ plot_prediction.py
@@ -11,7 +11,7 @@ def plot_prediction_radarscenes(model_name, data_root, checkpoint, index, npoint
         'pointnet2_seg_msg_radar': pointnet2_seg_msg_radar
     }
     Model = Models[model_name]
-    dataset_test = RadarScenes(data_root=data_root, split='validation', npoints=npoints, combined_frame_num=28)
+    dataset_test = RadarScenes(data_root=data_root, split='validation', npoints=npoints, combined_frame_num=28, augment=False, dp=False, normalize=False)
     device = torch.device('cuda')
     model = Model(dims, nclasses)
     model = model.to(device)
@@ -54,48 +54,48 @@ def plot_prediction_radarscenes(model_name, data_root, checkpoint, index, npoint
     xyz_pred_5 = xyz.numpy()[0, prediction[0] == 5, :]
 
     # plot the original point cloud and prediction in subplots
-    plt.figure(figsize=(10, 10))
-    if xyz_label_0.size != 0:
-        plt.scatter(xyz_label_0[:, 1], xyz_label_0[:, 0], c='red', s=5, label='CAR')
-    if xyz_label_1.size != 0:
-        plt.scatter(xyz_label_1[:, 1], xyz_label_1[:, 0], c='orange', s=5, label='PEDESTRIAN')
-    if xyz_label_2.size != 0:
-        plt.scatter(xyz_label_2[:, 1], xyz_label_2[:, 0], c='purple', s=5, label='PEDESTRIAN_GROUP')
-    if xyz_label_3.size != 0:
-        plt.scatter(xyz_label_3[:, 1], xyz_label_3[:, 0], c='green', s=5, label='TWO_WHEELER')
-    if xyz_label_4.size != 0:
-        plt.scatter(xyz_label_4[:, 1], xyz_label_4[:, 0], c='blue', s=5, label='LARGE_VEHICLE')
-    if xyz_label_5.size != 0:
-        plt.scatter(xyz_label_5[:, 1], xyz_label_5[:, 0], c='grey', s=1, label='STATIC')
-    plt.title('Ground Truth')
-    plt.legend()
-    # reverse the x axis
-    plt.xlim(plt.xlim()[::-1])
-    # add axis name
-    plt.xlabel('Y')
-    plt.ylabel('X')
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
 
-    plt.figure(figsize=(10, 10))
-    if xyz_pred_0.size != 0:
-        plt.scatter(xyz_pred_0[:, 1], xyz_pred_0[:, 0], c='red', s=5, label='CAR')
-    if xyz_pred_1.size != 0:
-        plt.scatter(xyz_pred_1[:, 1], xyz_pred_1[:, 0], c='orange', s=5, label='PEDESTRIAN')
-    if xyz_pred_2.size != 0:
-        plt.scatter(xyz_pred_2[:, 1], xyz_pred_2[:, 0], c='purple', s=5, label='PEDESTRIAN_GROUP')
-    if xyz_pred_3.size != 0:
-        plt.scatter(xyz_pred_3[:, 1], xyz_pred_3[:, 0], c='green', s=5, label='TWO_WHEELER')
-    if xyz_pred_4.size != 0:
-        plt.scatter(xyz_pred_4[:, 1], xyz_pred_4[:, 0], c='blue', s=5, label='LARGE_VEHICLE')
-    if xyz_pred_5.size != 0:
-        plt.scatter(xyz_pred_5[:, 1], xyz_pred_5[:, 0], c='grey', s=1, label='STATIC')
-    plt.title('Prediction')
-    plt.legend()
+    if xyz_label_0.size != 0:
+        ax1.scatter(xyz_label_0[:, 1], xyz_label_0[:, 0], c='red', s=5, label='CAR')
+    if xyz_label_1.size != 0:
+        ax1.scatter(xyz_label_1[:, 1], xyz_label_1[:, 0], c='orange', s=5, label='PEDESTRIAN')
+    if xyz_label_2.size != 0:
+        ax1.scatter(xyz_label_2[:, 1], xyz_label_2[:, 0], c='purple', s=5, label='PEDESTRIAN_GROUP')
+    if xyz_label_3.size != 0:
+        ax1.scatter(xyz_label_3[:, 1], xyz_label_3[:, 0], c='green', s=5, label='TWO_WHEELER')
+    if xyz_label_4.size != 0:
+        ax1.scatter(xyz_label_4[:, 1], xyz_label_4[:, 0], c='blue', s=5, label='LARGE_VEHICLE')
+    if xyz_label_5.size != 0:
+        ax1.scatter(xyz_label_5[:, 1], xyz_label_5[:, 0], c='grey', s=1, label='STATIC')
+    ax1.set_title('Ground Truth')
+    ax1.legend()
     # reverse the x axis
-    plt.xlim(plt.xlim()[::-1])
+    ax1.set_xlim(ax1.get_xlim()[::-1])
     # add axis name
-    plt.xlabel('Y')
-    plt.ylabel('X')
-    
+    ax1.set_xlabel('Y')
+    ax1.set_ylabel('X')
+
+    if xyz_pred_0.size != 0:
+        ax2.scatter(xyz_pred_0[:, 1], xyz_pred_0[:, 0], c='red', s=5, label='CAR')
+    if xyz_pred_1.size != 0:
+        ax2.scatter(xyz_pred_1[:, 1], xyz_pred_1[:, 0], c='orange', s=5, label='PEDESTRIAN')
+    if xyz_pred_2.size != 0:
+        ax2.scatter(xyz_pred_2[:, 1], xyz_pred_2[:, 0], c='purple', s=5, label='PEDESTRIAN_GROUP')
+    if xyz_pred_3.size != 0:
+        ax2.scatter(xyz_pred_3[:, 1], xyz_pred_3[:, 0], c='green', s=5, label='TWO_WHEELER')
+    if xyz_pred_4.size != 0:
+        ax2.scatter(xyz_pred_4[:, 1], xyz_pred_4[:, 0], c='blue', s=5, label='LARGE_VEHICLE')
+    if xyz_pred_5.size != 0:
+        ax2.scatter(xyz_pred_5[:, 1], xyz_pred_5[:, 0], c='grey', s=1, label='STATIC')
+    ax2.set_title('Prediction')
+    ax2.legend()
+    # reverse the x axis
+    ax2.set_xlim(ax2.get_xlim()[::-1])
+    # add axis name
+    ax2.set_xlabel('Y')
+    ax2.set_ylabel('X')
+
     plt.show()
 
 
